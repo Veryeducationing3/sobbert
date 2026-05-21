5fd9c8ef0517d4778c79661cc9eb2d137927906c
Veryeducationing3 <veryeducationing3@gmail.com>
2026-05-20 20:36:20 -0700

    Update azahar_libretro.js

--- Build/azahar_libretro.js
+++ Build/azahar_libretro.js
@@ -1,4 +1,4 @@
-<base href="https://cdn.jsdelivr.net/gh/veryeducationing3/sobbert/">
+<base href="https://cdn.jsdelivr.net/gh/veryeducationing3/lobbert/">
 
 var createAzaharModule = (function() {
   var _scriptDir = typeof document !== 'undefined' && document.currentScript ? document.currentScript.src : undefined;
